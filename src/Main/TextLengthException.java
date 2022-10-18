f4ba6d816a5643ab4793099dcb41cb3d7b28e207
Вадим <taratonovv8@gmail.com>
2022-10-18 14:27:57 +0300

    С2

--- src/Main/TextLengthException.java
+++ src/Main/TextLengthException.java
@@ -7,7 +7,7 @@ public class TextLengthException extends Exception{
 
     /**
      * Конструктор
-     * @param message сообщение об ошибке, выводиое при ошибке
+     * @param message сообщение об ошибке, выводимое при ошибке
      */
     public TextLengthException(String message) {
         super(message);
